252276b961a61971f688257ab9a825be73146603
ryanfrawley7 <15frawle7925@pit.edu>
2016-03-24 18:11:19 +0000

    Centered Content

--- main.js
+++ main.js
@@ -7,5 +7,19 @@ var content = document.createElement("div");
 document.body.appendChild(content);
 
 //put some words inside the content div
-
 content.innerHTML = "Hello world!";
+
+//increase text size of content div
+content.style.fontSize = "50px";
+
+//change background
+content.style.backgroundColor = "teal";
+
+//half the screen width
+content.style.width = "50%";
+
+//center the text in the content div
+content.style.textAlign = "center";
+
+//center the content div
+content.style.margin = "10% auto";
