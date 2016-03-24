8bfd3bfd7e3436d0af50e9434bef8fb16750398b
ryanfrawley7 <15frawle7925@pit.edu>
2016-03-24 17:52:35 +0000

    Made Content

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-alert();
+//let's build the html elements
+
+//create a div
+var content = document.createElement("div");
+
+//place the content div in the body
+document.body.appendChild(content);
+
+//put some words inside the content div
+
+content.innerHTML = "Hello world!";
